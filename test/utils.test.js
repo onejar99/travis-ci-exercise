28fe39441d3e9fc16863188d9c3ce9954daa2b51
OneJar <onejar99@gmail.com>
2019-01-03 21:44:50 +0800

    Modified the test cases.

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,12 @@
 const expect = require('expect');
 const utils = require('../src/utils');
 
-it('should add two numbers', () => {
+it('should add two numbers I', () => {
     let rlt = utils.add(11, 22);
     expect(rlt).toBe(33);
+});
+
+it('should add two numbers II', () => {
+    let rlt = utils.add(101, 0);
+    expect(rlt).toBe(101);
 });
